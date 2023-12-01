3aaf014171ee035e88f756e0edd4264497461eab
Ryan Monaghan <rmonaghan4@fordham.edu>
2023-12-01 12:40:36 -0500

    update

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello!');
+module.exports = {
+    plugins: [
+      require('tailwindcss')('./style.css'), // Adjust the file path here
+      require('autoprefixer'),
+      // ... other plugins
+    ]
+  }
+  
+  @tailwind base;
+@tailwind components;
+@tailwind utilities;
